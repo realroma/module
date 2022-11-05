55f66cc86d4e4674bd470cbd87b7245633b8fe2b
realroma <real3060@yandex.ru>
2022-11-04 21:07:16 -0400

    03.02.go without

--- 03.02.go
+++ 03.02.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main () {
+func add(arr [5]int, slise []int) bool {
+		for i := range arr {
+			for f := range slise {
+				y := slise[f]
+				if arr[i] == y {
+					return true
+				}
+			}
+			slise = append(slise, arr[i])
+		}
+		return false
+}
 
+func main () {
+	var arr = [...]int{1,2,3,4,5}
+	var arr2 = [...]int{1,1,3,4,4}
+	slise := make([]int, 0, 5)
+	fmt.Println(add(arr2, slise))
+	fmt.Println(add(arr, slise)) 
 }
