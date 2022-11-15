89da6c61c982940af793303bf385d0bedf8d0932
realroma <real3060@yandex.ru>
2022-11-15 11:58:14 -0500

    03.03.go

--- 03.03.go
+++ 03.03.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main () {
-	fmt.Println("a < b", "a" < "b")
+	var slise []string = []string{"1", "3", "2", "4"}
+	var slise2 []string
+	for i := range slise {
+		if slise2 == nil {
+			slise2 = append(slise2, slise[i])
+		} else {
+			for f := range slise2 {
+				fmt.Println(slise [i], slise[i] > slise2[f], slise[f])
+			}
+		slise2 = append(slise2, slise[i])
+		}
+	}
+	fmt.Println(slise, slise2)
 }
