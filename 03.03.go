e45e217ba0ad43f5a6e096994e8f4e5264a2638c
realroma <real3060@yandex.ru>
2022-11-15 12:06:25 -0500

    03.03.go

--- 03.03.go
+++ 03.03.go
@@ -6,14 +6,12 @@ func main () {
 	var slise []string = []string{"1", "3", "2", "4"}
 	var slise2 []string
 	for i := range slise {
-		if slise2 == nil {
-			slise2 = append(slise2, slise[i])
-		} else {
+		if slise2 != nil {
 			for f := range slise2 {
 				fmt.Println(slise [i], slise[i] > slise2[f], slise[f])
 			}
-		slise2 = append(slise2, slise[i])
 		}
+		slise2 = append(slise2, slise[i])
 	}
 	fmt.Println(slise, slise2)
 }
