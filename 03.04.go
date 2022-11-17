01790a04f12ae009a57d8b39cde715a2c6c590be
realroma <real3060@yandex.ru>
2022-11-17 16:21:38 -0500

    03 is end

--- 03.04.go
+++ 03.04.go
@@ -5,5 +5,17 @@ import (
 	)
 
 func main () {
-	fmt.Println("Тут будет выведен индетефикатор")
+	var str string = "съешь ещё этих мягких французких булок, да выпей чаю"
+	var maps map[string]int = map[string]int{}
+	s := []rune(str)
+	for _, f := range s {
+		g := string(f)
+		switch {
+			case g == " ":
+			case g == ",":
+			default:
+				maps[string (f)]++
+		}
+	}
+	fmt.Println(maps)
 }
---
+++ 04.01.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("% x", sample[i])
+	}
+	fmt.Println("\n", sample)
+}
