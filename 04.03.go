ca13f99a1e775dec4878b873c5a4762d968b9979
realroma <real3060@yandex.ru>
2023-04-02 06:17:38 +0300

    04.03.go

---
+++ 04.03.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+        "fmt"
+)
+
+var str string
+var l int
+
+type Checker struct {
+        items []interface{}
+}
+
+func (Checker *Checker) Add(items Checkable) {
+        Checker.items = append(Checker.items, items)
+}
+
+func Check(c *Checker){
+	for _, value := range c.items {
+		a, _ := value.(*GoMetrClient)
+		a.Health()
+	}
+}
+
+type Checkable interface {
+        Measurable
+        Ping()
+        GetID()
+        Health()
+}
+
+type Measurable interface {
+        GetMetrics()
+}
+
+func GetMertics(){
+
+}
+
+type GoMetrClient struct {
+        url string
+        timeout int
+}
+
+type HealthCheck struct {
+	ServisID string
+	status string
+}
+
+func (GMC *GoMetrClient) Ping() {
+
+}
+
+func (GMC *GoMetrClient) GetID() {
+
+}
+
+func (GMC *GoMetrClient) Health() {
+        a := getHealth()
+        str = str + "GetID: " + a.ServisID + " Health: " + a.status + "\n"
+	fmt.Println(str)
+}
+
+func (GMC *GoMetrClient) GetMetrics() {
+
+}
+
+func getHealth() *HealthCheck{
+	a := new(HealthCheck)
+	switch l {
+	case 0:
+		a = &HealthCheck{"1", "true"}
+	case 1:
+		a = &HealthCheck{"2", "false"}
+	case 2:
+		a = &HealthCheck{"3", "true"}
+	}
+	l++
+	return a
+}
+
+func main() {
+        items := &Checker{}
+        var Checkable1 Checkable
+        var Checkable2 Checkable
+        var Checkable3 Checkable
+	Checkable1 = &GoMetrClient{}
+	Checkable2 = &GoMetrClient{}
+	Checkable3 = &GoMetrClient{}
+        items.Add(Checkable1)
+        items.Add(Checkable2)
+        items.Add(Checkable3)
+	Check(items)
+}
+
