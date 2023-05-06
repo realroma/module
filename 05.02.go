9cbcab336eea3c9a791f1ae4930cf5f29f539b74
realroma <real3060@yandex.ru>
2023-05-06 04:28:13 +0300

    Stape save

--- 05.02.go
+++ 05.02.go
@@ -7,38 +7,37 @@ import (
 )
 
 func write(ch chan<- int) {
+	fmt.Println("Writing in chanel")
 	for i := 0; i <= 3; i++ { //Записываем в небуферизированный канал значения.
 		ch <- i
 	}
 	close(ch) //
 }
 
-func job(ch <-chan int, t *time.Timer) {
-	quit := make(chan int)
-	time.Sleep(3 * time.Second) //
+func read(ch <-chan int, t *time.Timer) {
+	time.Sleep(2 * time.Second)
+	fmt.Println("Seting for read")
 	select {
 	case <-t.C:
 		err := errors.New("Time is out.")
 		fmt.Println(err)
-	case <-quit:
 		return
 	case <-ch:
-		for i := range ch { //
+		for i := range ch {
 			fmt.Printf("Done %v.\n", i)
-		}
-		if !t.Stop() { //
-			<-t.C
 		}
 	}
 }
 
+func MyFunc () {
+	ch := make(chan int)
+	go write(ch)
+	timer := time.NewTimer(1 * time.Second)
+	go read(ch, timer)
+	time.Sleep(3 * time.Second)
+}
+
 func main() {
-	timer := time.NewTimer(1 * time.Second) //
-	ch := make(chan int)                    //
-	go write(ch)                            //
-
-	go job(ch, timer)
-
-	time.Sleep(3 * time.Second) //
-	fmt.Println("Done")
+	fmt.Println("MyFunc is starting")
+	MyFunc()
 }
