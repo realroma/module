665397b82925387862f374a85e3062a3ce457804
cPython+ <40072146+realroma@users.noreply.github.com>
2023-08-25 21:52:25 +0300

    Add files via upload

--- 06.02.go
+++ 06.02.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"flag"
 	"net/http"
 )
 
-func main() {
-	url := flag.String("url", "", "Path to server")
-	n := 1
-	flag.Parse()
-	fmt.Printf("Overall status is %v, with service_id %v mesql component is %v\n", n, n, *url)
+type Upstruc struct {
+	Status string
+	Service_id string
+	Checks checks
 }
 
-func HTTPClient() {
-	resp, _ := htt.Get
+type checks struct {
+	Ping_mysql ping_mysql
 }
+
+type ping_mysql struct {
+	Component_id string
+	Component_type string
+	Status string
+}
+
+func main() {
+	url := flag.String("url", "", "Path to server")
+	flag.Parse()
+	HTTPClient(*url)
+}
+
+func unmarshal(text string) {
+	var link Upstruc
+	json.Unmarshal([]byte(text), &link)
+	fmt.Printf("Overall status is %v, with service_id %v mesql component is %v\n", link.Status, link.Service_id, link.Checks.Ping_mysql.Status)
+}
+
+func HTTPClient(url string) {
+	resp, _ := http.Get(url)
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	a := scanner.Text()
+	unmarshal(a)
+}
+//How to use? Write with "-url http://localhost:8080/health".
---
+++ 06.02_Server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+//	"os"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"module/server"
+)
+
+func json(w http.ResponseWriter, req *http.Request) {
+	file, _ := ioutil.ReadFile("DataBase/db.json")
+	a := string(file)
+	fmt.Println(a)
+	fmt.Fprintf(w, a)
+}
+
+func main() {
+	http.HandleFunc("/health", json)
+	server.HTTPServer()
+}
