365c8df7085fcbd7e3b7ec75130db3f00ec2255a
realroma <real3060@yandex.ru>
2023-09-01 17:03:51 +0300

    Work with Database.

--- Service/intermal/models/models.go
+++ Service/intermal/models/models.go
@@ -1,13 +1,47 @@
 package models
 
+import	(
+	"os"
+	"io/ioutil"
+	"encoding/json"
+)
+
+func ErrCheck(err error) {
+	if err != nil {
+		fmt.Pritnln("Error:", err)
+		os.Exit(1)
+	}
+}
+
 type models struct {
+    Id int
     Name string
     Value string
     Date string
 }
 
+
 func DataBaseList() {}
 func DataBaseAdd() {}
-func DataBaseMarshal() {}
+
+func DataBaseMarshal(text []byte) text  {
+	structJson := models{
+		id: 0
+		name: "Some name",
+		value: "Some value",
+		timestamp: "Some time"
+	}
+	textJson, _ := json.MarshalIndent(structJson, text)
+	return textJson
+}
+
 func DataBaseUnmarshal() {}
-func DataBaseLoad(way string) {}
+
+func DataBaseLoad(writer string) {
+	file, err := os.Open("module/Service/intermal/bd/mosels.bd")
+	defer file.Close()
+	file, err := ioutil.ReadFile("module/Service/intermal/bd/models.bd")
+	ErrCheck(err)
+	file.Write(writer)
+	file.Sync()
+}
